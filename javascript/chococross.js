55ca2a6256352f4d5122d86dcc85539def513233
Amber Awadi <86750476+amberawadi@users.noreply.github.com>
2023-04-19 16:26:46 -0400

    Awadi Amber, 4/19/23
    Created JavaScript file chococross.js:
    -added functions to calculate total price
    -added functions for the snackbar messages
    -added functions for the image animation

---
+++ javascript/chococross.js
@@ -0,0 +1,63 @@
+//Amber Awadi
+//JavaScript page for chocolate croissant
+
+var navItems = document.querySelectorAll(".nav-item");
+for (var i = 0; i < navItems.length; i++) {
+   navItems[i].addEventListener("click", function() {
+      this.classList.add("active");
+   });
+}
+
+//mobile navbar function
+
+function mobileMenu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+//function for making the item image larger/normal on hover
+function bigImg(x) {
+  x.style.height = "400px";
+  x.style.width = "400px";
+}
+
+function normalImg(x) {
+  x.style.height = "300px";
+  x.style.width = "300px";
+}
+//function for pricing
+
+var topopt = parseFloat(document.getElementById("topopt").value);
+
+
+function topChoice(value) {
+	
+	topopt = parseFloat(value);
+	totalPrice()
+}
+
+function totalPrice() {
+	
+	if (topopt >= 0){
+		
+		subTotal = topopt;
+		document.getElementById("totalPrice").innerHTML = "$" + subTotal.toFixed(2);
+	}
+}
+
+//snackbar messages	
+function snackMessage() {
+  var x = document.getElementById("snackbar1");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+function snackMessage2() {
+  var y = document.getElementById("snackbar2");
+  y.className = "show";
+  setTimeout(function(){ y.className = y.className.replace("show", ""); }, 3000);
+}
