9141f88c0d2866efcc93b7ab934f4badf6ed4475
jusatanious <124219533+jusatanious@users.noreply.github.com>
2023-04-20 10:32:41 -0400

    Attempting to try and have javavalues transfer to sql database - Justin

--- javascript/ReesesPie.js
+++ javascript/ReesesPie.js
@@ -1,4 +1,4 @@
-
+/*
 //get prices if addons clicked
   var chocolate = 0;
   var whipped = 0;
@@ -42,4 +42,80 @@
       chocolate = 0;
       whipped = 0;
     });
+});
+*/
+var chocolate = 0;
+var whipped = 0;
+var total = 0;
+
+function addToCart() {
+  var sizeSelect = document.querySelector('select[name="size"]');
+  var selectedValue = sizeSelect.value;
+  var selectedFloat = parseFloat(selectedValue);
+  var addOnPrice = whipped + chocolate;
+  var totalPrice = selectedFloat + addOnPrice;
+
+  const mysql = require('mysql');
+
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'yourusername',
+    password: 'yourpassword',
+    database: 'yourdatabase'
+  });
+
+  const name = "Reese's Pie";
+  const image = "/images/reesespieimage.png";
+  const price = selectedFloat;
+
+  const sql = `INSERT INTO shoppingCart (name, image, price, AddOnPrice, totalPrice) VALUES (?, ?, ?, ?, ?)`;
+  const values = [name, image, price, addOnPrice, totalPrice];
+
+  connection.query(sql, values, (error, results, fields) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Values inserted successfully!');
+    }
+  });
+
+  connection.end();
+}
+
+$(document).ready(function(){
+  $('#ChocolateSauce').click(function(){
+      chocolate = parseInt($(this).val());
+      if($(this).text() == 'Added!') {
+          $(this).css('background-color', '');
+          $(this).text('Add');
+          chocolate = 0;
+      } else {
+          $(this).css('background-color', 'brown');
+          $(this).text('Added!');
+          chocolate = parseInt($(this).val());
+      }
+  });
+  $('#WhippedCream').click(function(){
+      whipped = parseInt($(this).val());
+      if($(this).text() == 'Added!') {
+          $(this).css('background-color', '');
+          $(this).text('Add');
+          whipped = 0;
+      } else {
+          $(this).css('background-color', 'brown');
+          $(this).text('Added!');
+          whipped = parseInt($(this).val());
+      }
+  });
+  $('.cancel-button').click(function(){
+    // Reset all buttons
+    $('.add-button').css('background-color', '');
+    $('.add-button').text('Add');
+    chocolate = 0;
+    whipped = 0;
+  });
+
+  $('#addToCartButton').click(function() {
+    addToCart();
+  });
 });
