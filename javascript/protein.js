eeea75a71924f4e973b4a7e808f72fa45ea5d875
jusatanious <124219533+jusatanious@users.noreply.github.com>
2023-04-22 14:48:50 -0400

    Added database adding to all page javascripts - Justin

--- javascript/protein.js
+++ javascript/protein.js
@@ -1,4 +1,36 @@
 
+function addToCart() {
+    var ProteinSelect = document.querySelector('select[name="size"]');
+    var selectedValue = ProteinSelect.value;
+    var selectedFloat = parseFloat(selectedValue);
+  
+    var addOnPrice = 0;
+    var totalPrice = selectedFloat;
+    alert('Selected Value: '+selectedFloat+' addons: '+addOnPrice+" totalprice: "+totalPrice)
+    const mysql = require('mysql');
+  
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      database: 'yourdatabase'
+    });
+  
+    const name = "Reese's Pie";
+    const image = "/images/reesespieimage.png";
+    const price = selectedFloat;
+  
+    const sql = `INSERT INTO shoppingCart (name, image, price, AddOnPrice, totalPrice) VALUES (?, ?, ?, ?, ?)`;
+    const values = [name, image, price, addOnPrice, totalPrice];
+  
+    connection.query(sql, values, (error, results, fields) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('Values inserted successfully!');
+      }
+    });
+  
+    connection.end();
+  }
 $(document).ready(function(){
     var selectedValue = $('select[name="size"]').val();
     
@@ -7,7 +39,7 @@ $(document).ready(function(){
         selectedValue = $(this).val();
     });
     $('.submit-button').click(function(){
-        alert(selectedValue);
+        addToCart();
     });
     //To cancel order/ reset values
     $('.cancel-button').click(function(){
--- javascript/smoothie.js
+++ javascript/smoothie.js
@@ -1,27 +1,64 @@
+
+var addOnPrice = 0;
+
+function addToCart(selectedValue,totalPrice) {
+  alert('Selected Value: ' + selectedValue + ' addons: ' + addOnPrice + " totalprice: " + totalPrice);
+  
+  const mysql = require('mysql');
+
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'yourdatabase'
+  });
+
+  const name = "Reese's Pie";
+  const image = "/images/reesespieimage.png";
+  const price = selectedValue;
+
+  const sql = `INSERT INTO shoppingCart (name, image, price, AddOnPrice, totalPrice) VALUES (?, ?, ?, ?, ?)`;
+  const values = [name, image, price, addOnPrice, totalPrice];
+
+  connection.query(sql, values, (error, results, fields) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Values inserted successfully!');
+    }
+  });
+
+  connection.end();
+}
+
 $(document).ready(function(){
-  var total = 0;
   $('.add-button').click(function(){
     // Deselect all buttons
     $('.add-button').css('background-color', '');
     $('.add-button').text('Select');
     $('.add-button').removeClass('selected');
+    
     var selectedValue = $('select[name="size"]').val();
-    var buttonValue = $(this).val();
-    total = (selectedValue * buttonValue).toFixed(2);
-//when we compound all totals the value will all be set to int in php else switch
+    var buttonValue = parseFloat($(this).val());
+    totalPrice = (selectedValue * buttonValue).toFixed(2);
+    
     // Select the clicked button
     $(this).addClass('selected');
     $(this).css('background-color', 'brown');
     $(this).text('Added!');
   });
+  
   $('.submit-button').click(function(){
-    alert(total);
+    var selectedValue = $('select[name="size"]').val();
+    var buttonValue = parseFloat($('.add-button.selected').val());
+    var totalPrice = (selectedValue * buttonValue).toFixed(2);
+    addToCart(selectedValue,totalPrice);
   });
+  
   $('.cancel-button').click(function(){
     // Reset all buttons
     $('.add-button').css('background-color', '');
     $('.add-button').text('Select');
     $('.add-button').removeClass('selected');
-    total = 0;
+    totalPrice = 0;
+    addOnPrice = 0;
   });
 });
