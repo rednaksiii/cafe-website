8e05d0d860dbf6596927b8b8657cf09de700cf02
cmcgrew98 <54917354+cmcgrew98@users.noreply.github.com>
2023-04-22 20:57:00 -0400

    McGrew, Clara updated index.php

--- php/index.php
+++ php/index.php
@@ -7,8 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://bootswatch.com/3/superhero/bootstrap.css"/>
-  <link rel="stylesheet" href="css/index_mobile.css" media="screen and (max-width:480px)" />
-  <link rel="stylesheet" href="css/index_desktop.css" media="screen and (min-width:481px)" />
+  <link rel="stylesheet" href="/css/index_mobile.css" media="screen and (max-width:480px)" />
+  <link rel="stylesheet" href="/css/index_desktop.css" media="screen and (min-width:481px)" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
